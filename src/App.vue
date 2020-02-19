c944e4a36287d6c38e8806c2aa99a19279efab21
Juliano Sirtori <julianosirtori@gmail.com>
2020-02-19 06:52:55 -0300

    Adequando o template gerado

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h1>{{titulo}}</h1>
+  <ul>
+    <li v-for="foto of fotos" :key="foto.titulo">
+      <img :src="foto.url" :alt="foto.titulo" />
+    </li>
+  </ul>
+  
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+      return {
+        titulo: 'Alurapic',
+        fotos: [
+          {
+            url: "https://abrilsuperinteressante.files.wordpress.com/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png",
+            titulo: 'cachorro'
+          },
+          {
+            url: "https://abrilsuperinteressante.files.wordpress.com/2016/03/cachorro.png",
+            titulo: 'cachorro2'
+          }
+        ]
+      }
+    }
+}
+</script>
+
+<style>
+</style>
